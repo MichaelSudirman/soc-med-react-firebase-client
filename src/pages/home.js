bd6e6fc0e5799a6d06823a1b987b63493e02e6e1
MichaelSudirman <astradon.michael@gmail.com>
2020-02-06 23:26:35 +0700

    added proxt to firebase backend, created base card for posts

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,41 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import axios from "axios";
+// Material UI imports
+import Grid from "@material-ui/core/Grid";
+// Components
+import Post from '../components/Post'
 export class home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Home page</h1>
-            </div>
-        )
-    }
+  state = {
+    posts: null
+  };
+  componentDidMount() {
+    axios
+      .get("/posts")
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          posts: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+  render() {
+    let recentPostsMarkUp = this.state.posts ? (
+      this.state.posts.map(post => <Post key={post.postId} post={post}/>)
+    ) : (
+      <p>Loading...</p>
+    );
+    return (
+      <Grid container spacing={16}>
+        <Grid item sm={8} xs={12}>
+          {recentPostsMarkUp}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile...</p>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
-export default home
+export default home;
