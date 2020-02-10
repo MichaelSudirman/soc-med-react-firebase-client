1c51279ee332b9da723d2ef412e4cdb5765cee72
MichaelSudirman <astradon.michael@gmail.com>
2020-02-10 14:35:34 +0700

    Updates

---
+++ precache-manifest.fbed774ddf4dd6f0f867ca1f8cd3f357.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f73b9634ff2b1b8d69444dcc1b9b1e3",
+    "url": "/soc-med-client/index.html"
+  },
+  {
+    "revision": "1bb078ec691ba176c302",
+    "url": "/soc-med-client/static/css/main.ac8c1c64.chunk.css"
+  },
+  {
+    "revision": "deaa726d525946edf281",
+    "url": "/soc-med-client/static/js/2.394ad71f.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/soc-med-client/static/js/2.394ad71f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb078ec691ba176c302",
+    "url": "/soc-med-client/static/js/main.8c2aa9cf.chunk.js"
+  },
+  {
+    "revision": "8fa0e42d60840b57cdec",
+    "url": "/soc-med-client/static/js/runtime-main.56f267c5.js"
+  }
+]);
