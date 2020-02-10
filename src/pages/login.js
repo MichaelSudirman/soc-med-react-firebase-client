a5a73558c7b05576b575eb307f037052d1b8877d
MichaelSudirman <astradon.michael@gmail.com>
2020-02-10 14:00:11 +0700

    finished login form feature

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import AppIcon from "../images/app.png";
 import axios from "axios";
+import { Link } from "react-router-dom";
 // Material UI imports
 import { withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 /*
   setting axios default url under the POST bug,
@@ -29,7 +31,16 @@ const styles = {
     margin: "10px auto 10px auto"
   },
   button: {
-    marginTop: 20
+    marginTop: 20,
+    position: "relative"
+  },
+  customError: {
+    color: "red",
+    fontSize: "0.8rem",
+    marginTop: 10
+  },
+  progress: {
+    position: "absolute"
   }
 };
 
@@ -62,7 +73,7 @@ class login extends Component {
         this.props.history.push("/");
       })
       .catch(err => {
-        console.log(err)
+        console.log(err);
         this.setState({
           errors: err.response.data,
           loading: false
@@ -110,14 +121,28 @@ class login extends Component {
               onChange={this.handleChange}
               fullWidth
             />
+            {errors.general && (
+              <Typography variant="body2" className={classes.customError}>
+                {errors.general}
+              </Typography>
+            )}
             <Button
               type="submit"
               variant="contained"
               color="primary"
               className={classes.button}
+              disabled={loading}
             >
               login
+              {loading && (
+                <CircularProgress size={30} className={classes.progress} />
+              )}
             </Button>
+            <br />
+            <small>
+              Dont Have an Account ? Sign Up
+              <Link to="/signup"> Here</Link>
+            </small>
           </form>
         </Grid>
         <Grid item sm />
