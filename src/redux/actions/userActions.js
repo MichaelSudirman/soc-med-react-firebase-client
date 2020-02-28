7d048e1ae1d04b9537b5e9c055c5b395550f2f59
MichaelSudirman <astradon.michael@gmail.com>
2020-02-28 22:52:10 +1000

    added logout and edit profile feature

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -7,6 +7,7 @@ import {
   LOADING_USER
 } from "../types";
 import axios from "axios";
+
 export const loginUser = (userData, history) => dispatch => {
   dispatch({ type: LOADING_UI });
   axios
@@ -24,6 +25,7 @@ export const loginUser = (userData, history) => dispatch => {
       });
     });
 };
+
 export const signupUser = (newUserData, history) => dispatch => {
   dispatch({ type: LOADING_UI });
   axios
@@ -41,11 +43,13 @@ export const signupUser = (newUserData, history) => dispatch => {
       });
     });
 };
+
 export const logoutUser = () => dispatch => {
   localStorage.removeItem("FBIdToken");
   delete axios.defaults.headers.common["Authorization"];
   dispatch({ type: SET_UNAUTHENTICATED });
 };
+
 export const getUserData = () => dispatch => {
   dispatch({ type: LOADING_USER });
   axios
@@ -58,15 +62,27 @@ export const getUserData = () => dispatch => {
     })
     .catch(err => console.log(err));
 };
-export const uploadImage = (formData) => (dispatch) => {
+
+export const uploadImage = formData => dispatch => {
   dispatch({ type: LOADING_USER });
   axios
-    .post('/user/image', formData)
+    .post("/user/image", formData)
     .then(() => {
       dispatch(getUserData());
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 };
+
+export const editUserDetails = userDetails => dispatch => {
+  dispatch({ type: LOADING_USER });
+  axios
+    .post("/user", userDetails)
+    .then(() => {
+      dispatch(getUserData());
+    })
+    .catch(err => console.log(err));
+};
+
 const setAuthorizationHeader = token => {
   const FBIdToken = `Bearer ${token}`;
   localStorage.setItem("FBIdToken", FBIdToken);
